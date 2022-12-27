8abbd9e58ccbc61ed712c979dd2e85a400a87c73
Mrinmoy Barai <urtruelymrinmoy1949@gmail.com>
2022-12-27 21:46:34 +0530

    some basic codings of JS

---
+++ basic.js
@@ -0,0 +1,83 @@
+//
+var a=6;
+var b=5;
+var c="I am a";
+//
+a=a+1;
+b=b+5;
+c=c + "string";
+console.log(a)
+console.log(b)
+console.log(c)
+
+var sum=10+10;
+console.log(sum)
+//
+var difference= 45-30;
+console.log(difference)
+//
+var myVar= 22;
+myVar=myVar+1;
+console.log(myVar)
+//
+var myVar= 22;
+myVar++
+console.log(myVar)
+//
+var remainder;
+remainder=11%3;
+console.log(remainder)
+//
+var a=2;
+var b=3;
+var c=4;
+a-= 4;
+b-= 6;
+c-= 2;
+console.log(a,b,c)
+//
+var myfirstName = "Mrinmoy";
+var mylastName = "Barai";
+console.log(myfirstName,mylastName)
+//
+var str = `'I am doing this "not just for you" but for "eveyone"'`;
+console.log(str)
+//
+var mystr="Do,what you can do." + "otherwise leave it."
+console.log(mystr)
+//
+var mystr="it is the first.";
+mystr+="may be the last time";
+console.log(mystr);
+//
+
+var myname="MB";
+var mystr="my name is " + myname + " and I am good ";
+ console.log(mystr);
+ //
+var anadjective="hardworking";
+var mystr="MB is very ";
+mystr+= anadjective;
+console.log(mystr);
+//
+var firstletterofmynamme="";
+var myname="Mrinmoy";
+firstletterofmyname=myname[4];
+
+console.log(firstletterofmyname)
+//
+var mystr="Lendon stans";
+mystr= "bendon stans";
+console.log(mystr)
+//
+var myname="Mrinmoy";
+secondlastletterofmyname=myname[myname.length - 2]
+console.log(secondlastletterofmyname)
+
+
+
+
+
+
+
+
---
+++ basic1.js
@@ -0,0 +1,173 @@
+function wordblanks(mynoun,myverb,myadjective,myadverb)
+{
+    var result="";
+    result+="The " + mynoun + " " + myverb + " " + myadjective + " " + myadverb
+    return result;
+}
+console.log(wordblanks("thunderbird","is","my","emotion"));
+//
+var myarray=["Mrinmoy",2];
+console.log(myarray)
+//
+var myarray=[["Mrinmoy",3],["Watchdog",5]];
+console.log(myarray)
+//
+var myarray=[3,6,9];
+var mydata=myarray[1];
+console.log(mydata)
+//
+var myarray=[3,6,9];
+myarray[1]=7;//it is now set
+console.log(myarray)
+//
+var myarray=[[1,2,3],[4,5,6],[[7,8,9],10,11,12]];
+var mydata=myarray[2][2];
+console.log(mydata)
+//
+var myarray=[["cat",23],["cow",87]];
+myarray.push(["berk","atan"]);
+console.log(myarray)
+//
+var myarray=[["cat",23],["cow",87]];
+myarray.shift();
+myarray.unshift(["SKR",09]);
+console.log(myarray)
+//
+function reusablefunction(){
+    console.log("hi Mimo");
+}
+reusablefunction()
+reusablefunction()
+reusablefunction()
+//
+function functionwithargs(a,b){
+    console.log(b/a)
+}
+functionwithargs(3,18)
+//
+function localscope(){
+    var myvar=18;
+    return myvar;
+}
+console.log(localscope()) 
+//
+var myouterwear="full sleeve";
+function myoutfit(){
+    var myouterwear="only sleeve";
+    console.log(myouterwear)
+}
+myoutfit()
+//
+function vense(num){
+    return num - 7;
+}
+console.log(vense(5))
+//
+var sum= 2;
+function something(){
+    sum+=3;
+    console.log(sum)
+}
+something()
+//
+var changed=0;
+function change(num){
+    return (num + 2)/4;
+}
+changed=change(0);
+console.log(changed)
+//
+function nextinline(array,item){
+    array.push(item);
+    return array.shift();
+}var array=[1,2,3,4,5,];
+
+console.log("before: " + array)
+console.log(nextinline(array,6))
+console.log("after: " + array)
+//
+function trueorfalse(istrue){
+    if (istrue){
+    return "it is true";
+    }
+    return "it is false";
+}
+console.log(trueorfalse(true))
+//
+function equality(val){
+    if (val == 13){
+        return "the value is equal";
+    }
+    return "the value is not equal";
+}
+console.log(equality(13))
+//
+function strictequality(val){
+    if (val === 13){
+        return "the value is equal";
+    }
+    return "the value is not equal";
+}
+console.log(strictequality('13'))
+//
+function nonequality(val){
+    if (val!= 98){
+        return "not equal";
+    }
+    return "equal";
+}
+console.log(nonequality(23))
+//
+function testgreater(val){
+    if (val>9.4){
+        return "over 9.4";
+    }
+    if (val>3.3)
+    {
+     return "over 3.3";
+     }
+     return "3.3 or under"
+    }
+    console.log(testgreater(5.6))
+    //
+    function testandlogic(val){
+        if (val <=50 && val >= 25){
+            return "it is true";
+        }
+        return "it is false";
+    }
+    console.log(testandlogic(36))
+    //
+    function logoroperator(val){
+        if (val<5.6 || val>17.5 ){
+            return "value outside";
+        }
+        return "value inside";
+    }
+    console.log(logoroperator(8.56))
+    //
+    function ifelse(val){
+    if(val < 6) {
+     result = "less than 6";
+    } else {
+          result = "greater than 6"
+         }
+       return result;
+        }
+        console.log(ifelse(4))
+        //
+    function elseif(val)  {
+        if (val >10){
+            return "greater than 10";
+        } else if (val<5) {
+            return "smaller than 5";
+        }
+        return "in between 5 and 10";
+        }
+    console.log(elseif(8)) 
+
+
+
+
+
+
